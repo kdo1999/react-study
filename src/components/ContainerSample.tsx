ae9adc164e06c0751bef6d586b4cbc3edcd4342d
kdo_1999/mac <kdo_1999@naver.com>
2024-10-11 17:14:23 +0900

    feat: 리액트 컴포넌트 학습

---
+++ src/components/ContainerSample.tsx
@@ -0,0 +1,22 @@
+const Container = (props: {title: string; children: React.ReactElement}) => {
+    const { title, children } = props
+    
+    return (
+        <div style={{ background: 'red' }}>
+            <span>{title}</span>
+            {/* props인 children을 삽입하면, 이 컴포넌트의 시작 태그와 종료 태그로 감싼 요소를 표시한다. */}
+            <div>{children}</div>
+        </div>
+    )
+}
+
+const Parent = () => {
+    return (
+        // Container를 사용할 때, 다른 요소를 감싸서 사용한다.
+        <Container title="Hello">
+           <p>이 부분이 배경색으로 둘러싸여 있습니다.</p> 
+        </Container>
+    )
+}
+
+export default Parent
