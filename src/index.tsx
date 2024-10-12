9eff083b690732d9b99d430ebc4370c5b0c41ced
kdo_1999/mac <kdo_1999@naver.com>
2024-10-12 20:43:11 +0900

    feat: useImperativeHandle 학습

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,7 @@ import UseMemoSample from './components/UseMemoSample'
 import Clock from './components/Clock'
 import UseContextSample from './components/UseContextSample'
 import UseRefSample from './components/UseRefSample'
+import UseImperativeHandle from './components/UseImperativeHandle'
 
 const root = ReactDOM.createRoot(
     // index.html에 있는 root를 ID로 가진 요소를 지정한다.
@@ -17,7 +18,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
     <React.StrictMode>
-        <UseRefSample />
+        <UseImperativeHandle />
     </React.StrictMode>,
 );
 
